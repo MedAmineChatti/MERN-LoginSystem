1f861854711ad9de7e8c1ef89c00fe794556ee7d
Mohamed Amine Chatti <85394302+MedAmineChatti@users.noreply.github.com>
2021-08-09 06:47:04 -0700

    Add files via upload

---
+++ client/src/api/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {DataProvider} from './GlobalState'
+import Header from './components/headers/Header'
+import MainPages from './components/mainpages/Pages'
+
+
+function App() {
+  return (
+    <DataProvider>
+     
+      <Router>
+        <div className="App">
+          <Header />
+          <br/><br/><br/><br/>
+          <MainPages />
+        </div>
+      </Router>
+    </DataProvider>
+  );
+}
+
+export default App;
